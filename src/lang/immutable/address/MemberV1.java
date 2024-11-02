b59dd72d74f23c89ee45c1d3d00ad9578b21af3a
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-11-02 23:28:15 +0900

    study at home

--- src/lang/immutable/address/MemberV1.java
+++ src/lang/immutable/address/MemberV1.java
@@ -4,4 +4,12 @@ public class MemberV1 {
 
     private String name;
     private Address address;
+
+    public MemberV1(String name, Address address) {
+        this.name = name;
+        this.address = address;
+    }
+
+
+
 }
