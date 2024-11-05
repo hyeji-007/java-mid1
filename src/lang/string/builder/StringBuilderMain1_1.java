3d4fa3af777e31aab2eea43275a6e6f0cec907b9
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-11-06 00:45:49 +0900

    study at home

--- src/lang/string/builder/StringBuilderMain1_1.java
+++ src/lang/string/builder/StringBuilderMain1_1.java
@@ -3,14 +3,25 @@ package lang.string.builder;
 public class StringBuilderMain1_1 {
 
     public static void main(String[] args) {
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder(); //가변
         sb.append("A");
         sb.append("B");
         sb.append("C");
         sb.append("D");
         System.out.println("sb = " + sb);
 
+        sb.insert(4, "Java");
+        System.out.println("sb = " + sb);
 
+        sb.delete(4,8);
+        System.out.println("delete = " + sb);
+
+        sb.reverse();
+        System.out.println("reverse = " + sb);
+
+        //StringBuilder -> String
+        String string = sb.toString();
+        System.out.println("string = " + string);
 
     }
 }
