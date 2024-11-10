d38cb54291bd6aaccbfbb86149f36b9f34bcf819
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-11-10 14:38:45 +0900

    11/10 study

---
+++ src/lang/string/test/TestString6.java
@@ -0,0 +1,10 @@
+package lang.string.test;
+
+public class TestString6 {
+
+    public static void main(String[] args) {
+        String str = "start hello hava, hello spring, hello jpa";
+        String key = "hello";
+
+    }
+}
