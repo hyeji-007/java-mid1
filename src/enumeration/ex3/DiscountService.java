db748ea3abe832958b7d0bc8249941a2ef70e2ba
Hyeji Kwon <kwonhyeji007@gmail.com>
2024-11-13 01:56:17 +0900

    11/12 study java-mid1

--- src/enumeration/ex3/DiscountService.java
+++ src/enumeration/ex3/DiscountService.java
@@ -1,15 +1,17 @@
 package enumeration.ex3;
 
+import static enumeration.ex3.Grade.*;
+
 public class DiscountService {
 
     public int discount(Grade classGrade, int price) {
         int discountPercent = 0;
 
-        if (classGrade == Grade.BASIC) {
+        if (classGrade == BASIC) {
             discountPercent = 10;
-        } else if (classGrade == Grade.GOLD) {
+        } else if (classGrade == GOLD) {
             discountPercent = 20;
-        }  else if (classGrade == Grade.DIAMOND) {
+        }  else if (classGrade == DIAMOND) {
             discountPercent = 30;
         } else {
             System.out.println("할인X");
